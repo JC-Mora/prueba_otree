c0b2cbfb65325f41f14d5534bc4ccd667cdb7962
JC-Mora <juan.mora@gmail.com>
2021-03-10 21:49:29 -0500

    cambio de prueba bargaining

--- bargaining/__init__.py
+++ bargaining/__init__.py
@@ -4,9 +4,8 @@ c = Currency
 
 
 doc = """
-This bargaining game involves 2 players. Each demands for a portion of some
-available amount. If the sum of demands is no larger than the available
-amount, both players get demanded portions. Otherwise, both get nothing.
+Este es una aplicacipn de prueba de oTree que consiste
+em que hay jugadores negociando entre si
 """
 
 
